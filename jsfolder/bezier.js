ff99b0e2077c8724b89ec395254d32fd351fc485
rhit-letscher <letscher@rose-hulman.edu>
2023-05-03 12:24:50 -0400

    finished bezier

---
+++ jsfolder/bezier.js
@@ -0,0 +1,71 @@
+const vector2d = require("vector2d.js");
+const globals = require("globals.js");
+const utils = require("utils.js");
+
+class Bezier {
+
+    constructor(_points){
+        this._points = _points;
+
+        this.approxlength = 0.0;
+        for (let i = 0; i < points.size() - 1; i++){
+            this.approxlength += points[i].getDistanceFrom(points[i + 1]);
+        }
+
+        init(approxlength);
+    
+    }
+
+    init(approxlength) {
+	// subdivide the curve
+	let ncurve = Math.floor((int)(approxlength / 4.0) + 2);
+	for (let i = 0; i < ncurve; i++)
+		curvePoints.push(pointAt(i / (double)(ncurve - 1)));
+
+	// find the distance of each point from the previous point
+	//curveDis = [];
+	let totalDistance = 0;
+	for (let i = 0; i < ncurve; i++) 
+	{
+		curveDis.push((i == 0) ? 0 : curvePoints[i].getDistanceFrom(curvePoints[i - 1]));
+		totalDistance += curveDis[i];
+	}
+}
+
+pointAt(t)
+{
+    let c = new Vector2d();
+	let n = points.length - 1;
+	for (let i = 0; i <= n; i++) 
+	{
+		let b = bernstein(i, n, t);
+		c.X += points[i].X * b;
+		c.Y += points[i].Y * b;
+	}
+	return c;
+}
+
+get curvePoints() {
+   return this.curvePoints;
+}
+
+get curveDistance(){
+    return this.curveDis;
+}
+
+get curvesCount(){
+    return this.ncurve;
+}
+
+get totalDistance(){
+    return this.totalDistance;
+}
+
+}
+module.exports = {Bezier};
+
+
+
+
+
+
