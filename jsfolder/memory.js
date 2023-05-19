107860dbd66f042315d9b4867b0a55f7f77f238b
Lukas <urbonal@rose-hulman.edu>
2023-05-18 23:06:26 -0400

    Fixed Memory calculations

--- jsfolder/memory.js
+++ jsfolder/memory.js
@@ -6,11 +6,11 @@ const tweakvars = require('./tweakvars.js');
 function GetApproachRelativeSize(time, hitTime, ar)
 {
 	if (hitTime < time) return 1;
-	else if (hitTime - AR2ms(ar) > time) return 0;
+	else if (hitTime - utils.AR2ms(ar) > time) return 0;
 	else
 	{
 		let diff = hitTime - time;
-		let interval = AR2ms(ar);
+		let interval = utils.AR2ms(ar);
 		return 1 + 3 * ( diff / interval);
 	}
 }
@@ -30,7 +30,7 @@ function CalculateMemory(beatmap)
 	let old = beatmap.hitObjects[0];
 	let combo = 0; // combo counter
 	
-	for (let i = 1; i < (int)(beatmap.hitObjects.length); i++)
+	for (let i = 1; i < Math.floor(beatmap.hitObjects.length); i++)
 	{
 		let cur = beatmap.hitObjects[i];
 		let memPoints = 0;
@@ -45,27 +45,27 @@ function CalculateMemory(beatmap)
 		
 		let sliderBonusFactor = 1;
 		if (utils.IsHitObjectType(old.type, globals.HITOBJECTTYPE.Slider))
-			sliderBonusFactor = mods.GetVar("Memory", "SliderBuff");
+			sliderBonusFactor = tweakvars.GetVar("Memory", "SliderBuff");
 		
 		let observable = false;
 		let helpPixels = 0; // it's easier to navigate when you see approaches / circle border (HD)
 		for (let j = i - 1; j > 0; j--)
 		{
 			let prev = beatmap.hitObjects[j];
-			if (cur.time - prev.time > AR2ms(beatmap.ar))
+			if (cur.time - prev.time > utils.AR2ms(beatmap.ar))
 				break;
 			if (!mods.hasMod(beatmap, globals.MODS.HD))
 			{
 				let size = GetApproachRelativeSize(prev.endTime, cur.time, beatmap.ar);
-				helpPixels = Math.floor(size * CS2px(beatmap.cs));
+				helpPixels = Math.floor(size * utils.CS2px(beatmap.cs));
 			}
 			else
 			{
 				let observableTime = cur.time;
-				observableTime = cur.time - Math.floor(static_cast<double>(AR2ms(beatmap.ar)) * 0.3); // hd dissapear interval
+				observableTime = cur.time - Math.floor((utils.AR2ms(beatmap.ar)) * 0.3); // hd dissapear interval
 				if (prev.time > observableTime)
 					continue; // dissapeared already
-				helpPixels = CS2px(beatmap.cs); // we can see more of a circle than just it's center point
+				helpPixels = utils.CS2px(beatmap.cs); // we can see more of a circle than just it's center point
 			}
 			if (IsObservableFrom(cur, observableDist + helpPixels, prev.pos))
 			{
@@ -79,18 +79,18 @@ function CalculateMemory(beatmap)
 			if (!mods.hasMod(beatmap, globals.MODS.HD))
 			{
 				let size = GetApproachRelativeSize(old.endTime, cur.time, beatmap.ar);
-				helpPixels = Math.floor(size * CS2px(beatmap.cs));
+				helpPixels = Math.floor(size * utils.CS2px(beatmap.cs));
 			}
 			else
 			{
-				helpPixels = CS2px(beatmap.cs); // we can see more of a circle than just it's center point
+				helpPixels = utils.CS2px(beatmap.cs); // we can see more of a circle than just it's center point
 			}
-			if (IsHitObjectType(cur.type, HITOBJECTTYPE.NewCombo) || IsHitObjectType(cur.type, HITOBJECTTYPE.ColorHax)) // only new combo changes
+			if (utils.IsHitObjectType(cur.type, globals.HITOBJECTTYPE.NewCombo) || utils.IsHitObjectType(cur.type, globals.HITOBJECTTYPE.ColorHax)) // only new combo changes
 			{
 				let dist = cur.pos.getDistanceFrom(old.endPoint);
 				// saving (distance/time) for spaced apart parts without followpoints
 				if (dist > observableDist + helpPixels)
-					memPoints = sliderBonusFactor * (dist / (double)(cur.time - old.time));
+					memPoints = sliderBonusFactor * (dist / (cur.time - old.time));
 			}
 			else
 			{
@@ -98,20 +98,22 @@ function CalculateMemory(beatmap)
 				// saving (distance/time) for spaced apart parts with followpoints
 				// treat parts with followpoints as easier ones
 				if (dist > observableDist + helpPixels)
-					memPoints = sliderBonusFactor * GetVar("Memory", "FollowpointsNerf") * (dist / (double)(cur.time - old.time));
+					memPoints = sliderBonusFactor * tweakvars.GetVar("Memory", "FollowpointsNerf") * (dist / (cur.time - old.time));
 			}
 		}
 
 		// count combo
-		if (IsHitObjectType(cur.type, globals.HITOBJECTTYPE.Normal) || IsHitObjectType(cur.type, globals.HITOBJECTTYPE.Spinner))
+		if (utils.IsHitObjectType(cur.type, globals.HITOBJECTTYPE.Normal) || utils.IsHitObjectType(cur.type, globals.HITOBJECTTYPE.Spinner))
 			combo++;
-		else if (IsHitObjectType(cur.type, globals.HITOBJECTTYPE.Slider))
-			combo += cur.ticks.size() + 2;
+		else if (utils.IsHitObjectType(cur.type, globals.HITOBJECTTYPE.Slider))
+			combo += cur.ticks.length + 2;
 
 		old = cur; // save previous object
 		totalMemPoints += memPoints;
 	}
 	beatmap.skills.memory = totalMemPoints;
-	beatmap.skills.memory = GetVar("Memory", "TotalMult") * pow(beatmap.skills.memory, GetVar("Memory", "TotalPow"));
+	beatmap.skills.memory = tweakvars.GetVar("Memory", "TotalMult") * Math.pow(beatmap.skills.memory, tweakvars.GetVar("Memory", "TotalPow"));
 	return beatmap.skills.memory;
 }
+
+module.exports = { CalculateMemory }
